6190035795ca91ca5f409c2418fa6e9b9f58a19a
spundev <spundev@gmail.com>
2025-10-31 23:56:50 +0100

    Fix inconsistent tab size

--- app/components/syncSettings/syncSettings.tsx
+++ app/components/syncSettings/syncSettings.tsx
@@ -4,29 +4,29 @@ import { TimestampInput } from "../timestampInput/timestampInput"
 import { getTimestampInMillis, ZERO_TS } from "~/models/Timestamp";
 
 interface SyncSettingsProps {
-    onSyncRequest: (millis: number) => void;
+  onSyncRequest: (millis: number) => void;
 }
 
 export function SyncSettings({ onSyncRequest }: SyncSettingsProps) {
 
-    const [timestamp, setTimestamp] = useState(ZERO_TS)
+  const [timestamp, setTimestamp] = useState(ZERO_TS)
 
-    const handleAddTime = () => {
-        onSyncRequest(getTimestampInMillis(timestamp))
-    }
+  const handleAddTime = () => {
+    onSyncRequest(getTimestampInMillis(timestamp))
+  }
 
-    const handleRemoveTime = () => {
-        onSyncRequest(-getTimestampInMillis(timestamp))
-    }
+  const handleRemoveTime = () => {
+    onSyncRequest(-getTimestampInMillis(timestamp))
+  }
 
-    return (
-        <section className="card">
-            <h3>Sync correction</h3>
-            <TimestampInput timestamp={timestamp} onTimestampChange={setTimestamp} />
-            <div className="actions">
-                <TextButton onClick={handleAddTime}>Add time</TextButton>
-                <TextButton onClick={handleRemoveTime}>Remove time</TextButton>
-            </div>
-        </section>
-    )
+  return (
+    <section className="card">
+      <h3>Sync correction</h3>
+      <TimestampInput timestamp={timestamp} onTimestampChange={setTimestamp} />
+      <div className="actions">
+        <TextButton onClick={handleAddTime}>Add time</TextButton>
+        <TextButton onClick={handleRemoveTime}>Remove time</TextButton>
+      </div>
+    </section>
+  )
 }
