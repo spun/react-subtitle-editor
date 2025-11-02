585ee17e268a10316b8f636b7632f9972e116e7c
spundev <spundev@gmail.com>
2025-11-02 18:05:30 +0100

    Remove tailwindcss

--- app/root.tsx
+++ app/root.tsx
@@ -65,14 +65,16 @@ export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
   }
 
   return (
-    <main className="pt-16 p-4 container mx-auto">
-      <h1>{message}</h1>
-      <p>{details}</p>
-      {stack && (
-        <pre className="w-full p-4 overflow-x-auto">
-          <code>{stack}</code>
-        </pre>
-      )}
+    <main>
+      <div>
+        <h1>{message}</h1>
+        <p>{details}</p>
+        {stack && (
+          <pre>
+            <code>{stack}</code>
+          </pre>
+        )}
+      </div>
     </main>
   );
 }
